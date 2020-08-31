b61154b57e936000c3fe41c5069cbcc878d304f1
sourav kumar <souravs_1999@rediffmail.com>
2020-08-31 11:09:13 +0530

    Update outlier.py

--- outlier/outlier.py
+++ outlier/outlier.py
@@ -1,8 +1,3 @@
-"""
-@ author : sourav kumar (101883068)
-@ made for UCS633 - PROJECT - II
-@ Timestamp : 2020 / 2 / 6 , 12 : 10 : 13 . 836206
-"""
 import numpy as np
 import pandas as pd
 import sys
